816124dbe4d6a87c0916a653383657edfb9afe75
Karishma <tikkysood@yahoo.com>
2014-12-11 08:16:48 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -4,4 +4,49 @@ public class Employee extends Person
   private double hourlyWage;
   private int hoursWorked;
   private String jobTitle;
-  
+  private Manager manager;
+  public double getWage()
+  {
+    retrurn hourlyWage;
+  }
+  public int getHours()
+  {
+    return hoursWorked;
+  }
+  public double getWeeklyWage()
+  {
+    return hoursWorked*hourlyWage*7;
+  }
+  public double getYearlySalary()
+  {
+    return getWeeklyWage()*52;
+  }
+  public String getJobTitle()
+  {
+    return jobTitle;
+  }
+  public String toString()
+  {
+    return lastName", "+firstName+": "+jobTitle"@"+hourlyWage;
+  }
+  public setWage(double newWage)
+  {
+    hourlyWage=newWage;
+  }
+  public setHours(newHours)
+  {
+    hoursWorked=newHours;
+  }
+  public setTitle(newTitle)
+  {
+    jobTitle=newTitle;
+  }
+  public getRaise()
+  {
+    hourlyWage++;
+  }
+  public setManager(Manager newManager)
+  {
+    manager=newManager;
+  }
+}
