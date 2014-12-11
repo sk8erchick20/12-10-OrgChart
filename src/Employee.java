3fcbfa16cbc0fa3b26ff49ed61d091d11653d11f
Karishma <tikkysood@yahoo.com>
2014-12-10 20:58:22 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,7 @@
 // class to represent an emplayee - inherits Person
+public class Employee extends Person
+{
+  private double hourlyWage;
+  private int hoursWorked;
+  private String jobTitle;
+  
